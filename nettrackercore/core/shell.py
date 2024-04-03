e0300d2614695fa97f89f04d42475802cb9a632c
VLB3R70 <vlb3r70@protonmail.com>
2024-04-03 21:48:51 +0200

    feat(core): Añadidos más comandos cuando se usa el objeto Scanner
    Se ha añadido funcionalidad a los comandos 'set' y 'scan'.
    - Con 'set' establecemos los valores a los parámetros necesarios para el escaneo; el objetivo, los puertos y 'otros' son valores de tipo string que el usuario modifica directamente.
    El resto de parámetros se deben de establecer entre verdaderos o falsos. Cuando son verdaderos se añaden parámetros permitidos por Nmap a la variable 'other'.
    
    - Con 'scan' se procede a hacer el escaneo con el objeto Scanner y se establecen como parámetros de escaneo los introducidos por el usuario.
    
    Author: VLB3R70

--- nettrackercore/core/shell.py
+++ nettrackercore/core/shell.py
@@ -1,9 +1,10 @@
 import signal
 
-from rich import print
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.prompt import Prompt
+
+from scanner import Scanner
 
 console = Console()
 
@@ -26,6 +27,9 @@ class Shell:
 |_|\_||___|  |_|          |_|  |_|_\|_|_| \___||_|\_\|___||_|_\\
 """
 
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.handler)
+
     def handler(self, signum=None, frame=None):
         console.print("\n[magenta][bold]Goodbye! 👋")
         exit(1)
@@ -33,40 +37,74 @@ class Shell:
     def help(self):
         markdown = Markdown(HELP)
         print(markdown)
-        self.main_menu()
 
-    def run(self, args):
-        pass
-
+    # se necesita optimizar esta parte del código
     def scanner(self):
+        sc = Scanner()
+        target = ""
+        ports = ""
+        other = ""
+        silent = False
+        tcp = False
+        udp = False
+        os = False
+        sudo = False
+
         while True:
-            option = Prompt.ask(PROMPT + "([bold][red]scanner[/red][/bold])")
+            option = Prompt.ask(PROMPT + "([bold red]scanner[/bold red])")
             if option == "exit":
                 exit(0)
             elif option == "tracker":
                 self.tracker()
+            elif option.startswith("set"):
+                option = option.split()
+                if option[1] == "target":
+                    target = str(option[2])
+                elif option[1] == "ports":
+                    ports = str(option[2])
+                elif option[1] == "other":
+                    other = str(option[2])
+                elif option[1] == "silent":
+                    silent = option[2]
+                elif option[1] == "tcp":
+                    tcp = option[2]
+                elif option[1] == "udp":
+                    udp = option[2]
+                elif option[1] == "os":
+                    os = option[2]
+            elif option == "scan":
+                if silent:
+                    other += "-sS "
+                elif tcp:
+                    other += "-sT "
+                elif udp:
+                    other += "-sU "
+                elif os:
+                    other += "-O "
+                    sudo = True
+
+                sc.scan(target=target, ports=ports, params=other, sudo=sudo)
 
     def tracker(self):
         while True:
-            option = Prompt.ask(PROMPT + "([bold][red]tracker[/red][/bold])")
+            option = Prompt.ask(PROMPT + "([bold red]tracker[/bold red])")
             if option == "exit":
                 exit(0)
             elif option == "scanner":
                 self.scanner()
 
     def main_menu(self):
-        signal.signal(signal.SIGINT, self.handler)
-
         print(self.LOGO)
-        option = Prompt.ask(PROMPT)
-        if option == "help":
-            self.help()
-        elif option == "scanner":
-            self.scanner()
-        elif option == "tracker":
-            self.tracker()
-        elif option == "exit":
-            exit(0)
+        while True:
+            option = Prompt.ask(PROMPT)
+            if option == "help":
+                self.help()
+            elif option == "scanner":
+                self.scanner()
+            elif option == "tracker":
+                self.tracker()
+            elif option == "exit":
+                exit(0)
 
 
 shell = Shell()
