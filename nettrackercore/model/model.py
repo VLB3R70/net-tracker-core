cd6bc8975712f5cbe56756071fd92526943e7cb7
VLB3R70 <vlb3r70@protonmail.com>
2024-04-18 19:53:42 +0200

    feat(model)!: Nuevo objeto que representa los servicios escaneados

--- nettrackercore/model/model.py
+++ nettrackercore/model/model.py
@@ -1,4 +1,10 @@
-from mongoengine import Document, StringField, ListField, DictField, EmbeddedDocument, EmbeddedDocumentField, IntField
+from mongoengine import Document, StringField, ListField, EmbeddedDocument, EmbeddedDocumentField, IntField
+
+
+class Service(EmbeddedDocument):
+    name = StringField(required=True)
+    port = IntField(required=True)
+    protocol = StringField(required=True)
 
 
 class Device(EmbeddedDocument):
@@ -6,8 +12,7 @@ class Device(EmbeddedDocument):
     device_name = StringField(required=True)
     address = StringField(required=True)
     netmask = IntField(required=True)
-    gateway = StringField(required=True)
-    services = ListField(DictField(required=True))
+    services = ListField(EmbeddedDocumentField(Service))
     os_type = StringField(required=True)
 
 
@@ -15,5 +20,6 @@ class Network(Document):
     network_id = StringField(primary_key=True)
     network_name = StringField(required=True)
     address = StringField(required=True)
+    gateway = StringField(required=True)
     subnet_mask = IntField(required=True)
     devices = ListField(EmbeddedDocumentField(Device), required=True)
