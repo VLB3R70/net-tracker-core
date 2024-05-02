9e278b8b89903ac5f21915701d0fff4907eaf28b
VLB3R70 <vlb3r70@protonmail.com>
2024-05-02 11:30:51 +0200

    feat(core): Mejorada la lógica de las excepciones personalizadas

--- nettrackercore/core/exceptions.py
+++ nettrackercore/core/exceptions.py
@@ -3,8 +3,10 @@ class NettrackerException(Exception):
     Excepción base para todas las excepciones del programa.
     """
 
-    def __init__(self, message):
-        super().__init__(self, message)
+    def __init__(self, message="System exception", code=1, *args, **kwargs):
+        self.message = message
+        self.code = code
+        super().__init__(message, *args, **kwargs)
 
 
 class InvalidArgumentsException(NettrackerException):
@@ -12,25 +14,26 @@ class InvalidArgumentsException(NettrackerException):
     Esta excepción se lanza cuando un argumento introducido por el usuario no es válido.
     """
 
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message="One or more arguments are invalid.", code=2, *args, **kwargs):
+        super().__init__(message, code, *args, **kwargs)
 
 
 class ExecutionError(NettrackerException):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-
-class IllegalArgumentException(InvalidArgumentsException):
     """
-    Esta excepción se lanza cuando el usuario ha introducido un argumento que no debería introducir. La diferencia entre
-    :py:class:`InvalidArgumentsException` e :py:class:`IllegalArgumentException` es que: la primera corresponde a un
-    error del usuario y la segunda a una opción no permitida por la lógica del programa.
+    Esta excepción se lanza cuando ocurre un error durante la ejecución del programa.
     """
 
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message="An error occurred during the execution of the program.", code=3, *args, **kwargs):
+        super().__init__(message, code, *args, **kwargs)
+
+
+class IllegalArgumentException(NettrackerException):
+    """
+    Esta excepción se lanza cuando el usuario ha introducido un argumento que no debería introducir.
+    """
+
+    def __init__(self, message="One or more arguments are illegal.", code=4, *args, **kwargs):
+        super().__init__(message, code, *args, **kwargs)
 
 
 class InvalidPortsException(NettrackerException):
@@ -38,8 +41,8 @@ class InvalidPortsException(NettrackerException):
     Esta excepción se lanza cuando los puertos introducidos por el usuario no son válidos.
     """
 
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message="The ports introduced are invalid.", code=5, *args, **kwargs):
+        super().__init__(message, code, *args, **kwargs)
 
 
 class InvalidAddressException(NettrackerException):
@@ -47,5 +50,5 @@ class InvalidAddressException(NettrackerException):
     Esta excepción se lanza cuando la dirección IP introducida por el usuario no es válida.
     """
 
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message="The address or addresses introduced are invalid.", code=6, *args, **kwargs):
+        super().__init__(message, code, *args, **kwargs)
